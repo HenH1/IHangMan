99f19bd7f385c7a8d3ad322c5829ef34f1d4ffd4
amit2n <amit2n@gmail.com>
2016-07-10 14:06:40 +0300

    lunchscreen main view - login firebase

---
+++ IHangMan/DataSerivce.swift
@@ -0,0 +1,16 @@
+//
+//  DataSerivce.swift
+//  IHangMan
+//
+//  Created by Amit Nahmani on 7/9/16.
+//  Copyright © 2016 Amit Nahmani. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class DataSerivce {
+    static let ds DataService()
+    
+    private var _REF_BASE = FireBase(url: "https")
+}
